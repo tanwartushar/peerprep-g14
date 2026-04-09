06fd1ec8f95bce001b1449c3050f8cbfcf00bb39
YWY2002 <wenyuanyik@gmail.com>
2026-04-09 12:40:12 +0800

    took out redis

--- services/question-service/handler/handler.go
+++ services/question-service/handler/handler.go
@@ -12,14 +12,14 @@ import (
 	// "github.com/tgonet/peerprep-g14/services/question-service/internal/database"
 	"github.com/tgonet/peerprep-g14/services/question-service/question/repository"
 	"go.mongodb.org/mongo-driver/v2/mongo"
-	"github.com/redis/go-redis/v9"
+	// "github.com/redis/go-redis/v9"
 	// "go.mongodb.org/mongo-driver/v2/mongo/options"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
 	DB *mongo.Client
-	Cache *redis.Client
+	// Cache *redis.Client
     QuestSvc *repository.QuestionService
 }
 
